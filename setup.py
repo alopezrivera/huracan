dfcbb1b4b800f33999cf322975a10ae247e3e08d
Antonio Lopez Rivera <antonlopezr99@gmail.com>
2021-08-28 20:42:53 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="huracan",
+    version="0.0.d0",
+    author="Antonio Lopez Rivera",
+    author_email="antonlopezr99@gmail.com",
+    description="Open source, 0-dimensional, object-oriented airbreathing engine modelling package for preliminary analysis and design of airbreathing engines, divulgation and educational purposes.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alopezrivera/huracan",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "Python-Alexandria",
+        "mpl_plotter"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL 2.0",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
