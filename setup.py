87714460eb240631cee48a89c562a01274272a6c
Antonio Lopez Rivera <antonlopezr99@gmail.com>
2021-08-28 21:26:20 +0200

    Alpha version code

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="huracan",
-    version="0.0.d0",
+    version="0.0.alpha0",
     author="Antonio Lopez Rivera",
     author_email="antonlopezr99@gmail.com",
     description="Open source, 0-dimensional, object-oriented airbreathing engine modelling package for preliminary analysis and design of airbreathing engines, divulgation and educational purposes.",
