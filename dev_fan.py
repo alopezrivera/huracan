48085b4d3a0996439f6664c6e9864b8474ed0133
Antonio Lopez Rivera <antonlopezr99@gmail.com>
2021-09-01 16:27:08 +0200

    Fan syntax

---
+++ dev_fan.py
@@ -0,0 +1,12 @@
+fan  =
+turb =
+
+shaft(fan, turb)
+
+gas-fan = turb-nozzle  -> fan bypass parallel to engine, reunites at last stage
+gas-fan < turb-nozzle  -> fan bypass separates from engine
+
+gas-fan         |
+gas2-inlet-turb | -> gases do not mix
+
+
