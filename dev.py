6bacef6a9e6eff8488f405989acb6c26587d6f0d
Antonio Lopez Rivera <antonlopezr99@gmail.com>
2021-09-02 17:10:46 +0200

    Update dev.py

--- dev.py
+++ dev.py
@@ -30,18 +30,27 @@ p  = power_plant(w=970000, eta_g=0.7, eta_c=0.98)
 shaft1 = shaft(c1, t2,    eta=0.99)
 shaft2 = shaft(c2, t1, p, eta=0.99)
 
-stream = g-pr-i-c1-c2-cc-t1-t2-ab-n
+stream = g-pr-i-c1-c2-cc-t1-t2
 
-stream.run()
+# stream.run()
 
-model_t0 = stream.t0()
-model_p0 = stream.p0()
+# model_t0 = stream.t0()
+# model_p0 = stream.p0()
 
 # stream.plot_T_p(show=True, color='purple')
 # stream.plot_p_v(show=True, color='orange')
 
-assert stream.v_exit() - 840.3353 < 10e-5, stream.v_exit() - 840.3353
+# assert stream.v_exit() - 840.3353 < 10e-5, stream.v_exit() - 840.3353
 
-print(stream.v_exit())
-print(stream.A_exit())
-print(stream.thrust())
+# print(stream.v_exit())
+# print(stream.A_exit())
+# print(stream.thrust())
+
+m, d = stream*0.4
+
+m-t2-ab-n
+d-n
+
+mm = m-d
+
+mm.run()
