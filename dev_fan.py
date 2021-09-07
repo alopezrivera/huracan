3bfe39fd11d2fb6968af4057795db64b0e1f8e70
Antonio Lopez Rivera <antonlopezr99@gmail.com>
2021-09-07 22:39:21 +0200

    Cleanup

--- dev_fan.py
+++ dev_fan.py
@@ -6,7 +6,3 @@ shaft(fan, turb)
 gas-fan = turb-nozzle  -> fan bypass parallel to engine, reunites at last stage
 gas-fan < turb-nozzle  -> fan bypass separates from engine
 
-gas-fan         |
-gas2-inlet-turb | -> gases do not mix
-
-
