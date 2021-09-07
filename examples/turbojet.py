a8ede4861aed0a8fff868bd2a7731299cd24a891
Antonio Lopez Rivera <antonlopezr99@gmail.com>
2021-09-07 19:13:21 +0200

    Update turbojet.py

--- examples/turbojet.py
+++ examples/turbojet.py
@@ -46,8 +46,8 @@ stream = g-i-c1-c2-cc-t1-t2-ab-n
 stream.run()
 
 stream.plot_T_p(show=True, color='purple')
-stream.plot_p_v(show=True, color='orange')
+stream.plot_p_V(show=True, color='orange')
 
 print(stream.v_exit())
 print(stream.A_exit())
-print(stream.thrust())
+print(stream.flow_thrust())
