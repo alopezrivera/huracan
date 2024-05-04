35cbedd7b9108c1bc8beb3549304f9ffe8769bb3
Antonio López Rivera <antonlopezr99@gmail.com>
2024-05-04 12:29:04 +0200

    Updated copyright tags

--- huracan/components/static.py
+++ huracan/components/static.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: © 2021 Antonio López Rivera <antonlopezr99@gmail.com>
+# SPDX-FileCopyrightText: © 2024 Antonio López Rivera <antonlopezr99@gmail.com>
 # SPDX-License-Identifier: vGPL-3.0-only
 
 """
--- tests/verification/turboprop/__init__.py
+++ tests/verification/turboprop/__init__.py
@@ -1,2 +1,2 @@
-# SPDX-FileCopyrightText: © 2021 Antonio López Rivera <antonlopezr99@gmail.com>
+# SPDX-FileCopyrightText: © 2024 Antonio López Rivera <antonlopezr99@gmail.com>
 # SPDX-License-Identifier: MPL-2.0
